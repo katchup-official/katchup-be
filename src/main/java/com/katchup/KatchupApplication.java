e0befd07abf5b917d725eaeab67daa0b3aac7cdf
Subin Cho <109792191+chsubinn@users.noreply.github.com>
2025-08-08 18:57:49 +0900

    Initial commit

---
+++ src/main/java/com/katchup/KatchupApplication.java
@@ -0,0 +1,13 @@
+package com.katchup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatchupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatchupApplication.class, args);
+	}
+
+}
