1aa2af6c67661f4a4f1d64e991e3463f5007814f
Subin Cho <chosubinn@ewhain.net>
2025-08-11 15:25:46 +0900

    KU-2/chore: profiile 분리

--- src/main/java/com/katchup/KatchupApplication.java
+++ src/main/java/com/katchup/KatchupApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KatchupApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KatchupApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(KatchupApplication.class, args);
+    }
 }
