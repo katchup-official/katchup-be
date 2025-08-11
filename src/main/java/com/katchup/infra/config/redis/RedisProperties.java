a47bc735d84f6f39cfa951202804cf035a2c08ee
Subin Cho <chosubinn@ewhain.net>
2025-08-11 21:28:29 +0900

    KU-2/chore: redis 설정

---
+++ src/main/java/com/katchup/infra/config/redis/RedisProperties.java
@@ -0,0 +1,6 @@
+package com.katchup.infra.config.redis;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("spring.data.redis")
+public record RedisProperties(String host, int port, String password) {}
